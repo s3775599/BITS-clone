a5159cd0b24e7a054cda735b83e2fb2ef6324f81
Paul <paulatkins88@gmail.com>
2019-10-04 22:20:33 +1000

    Main game with dialog system
    This is the original project with dialog system integrated. Press 'h' to start the dialog (we will no doubt change this around, but this shows it working)

--- NightWatch/Console_miniGame.gd
+++ NightWatch/Console_miniGame.gd
@@ -9,3 +9,7 @@ func _process(delta):
 	# game will quit if user hits escape - might wanna take this out after testing phase
 	if Input.is_action_pressed("ui_exit"):
       get_tree().quit()
+	
+	# If player presses 'h' which is mapped to ui_help the dialog system will load
+	if Input.is_action_just_released("ui_help"):
+		get_tree().change_scene("res://Main.tscn")
--- NightWatch/buttons.gd
+++ NightWatch/buttons.gd
@@ -2,10 +2,15 @@ extends Area2D
 
 # sequence is the array to match
 var sequence = [3,5,6]
+
 # clicked is the array the player is filling with their 'clicks'
 var clicked = [0,0,0]
+
 # the position in the array
 var clickPosition = 0
+
+# has the puzzle been solved controlling whether the user can click on sprites anymore?
+var solved = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,22 +20,31 @@ func _ready():
 # This will check if the sequence has been matched, I would of thought there was a better way to see if arrays are duplicates
 # apparrently not.
 func _process(delta):
-	$Control/CodeLabel.text = str(clicked[0]) + ',' + str(clicked[1]) + ',' + str(clicked[2])
+	#$Control/CodeLabel.text = str(clicked[0]) + ',' + str(clicked[1]) + ',' + str(clicked[2])
 	#if clickPosition < sequence.size():
 	#	if clicked[0] == sequence[0] and clicked[1] == sequence[1] and clicked[2] == sequence[2]:
 	#		$Control/WinLabel.visible = true
 	# UNCOMMENT if we want to limit the amount of times they can try.
 	#elif clickPosition > sequence.size():
 	#	$Control/FailLabel.visible = true
+	
 	if clicked[0] == sequence[0] and clicked[1] == sequence[1] and clicked[2] == sequence[2]:
-			$Control/WinLabel.visible = true
+		#$Control/WinLabel.show()
+		solved = true		
+	else:
+		#$Control/WinLabel.hide()
+		solved = false
+	if clicked[0] == 0 and clicked[1] == 1 and clicked[2] == 0:
+		$Katyusha.play()
+	print(clicked)
 
 
 # This handles the input, it will cycle the current clicked position in the array
 func _on_buttons_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
-			if clickPosition < sequence.size():
+			# This condition checks for array position and if the puzzle has been solved
+			if clickPosition < sequence.size() && !solved:
 				if shape_idx == 0:
 					$topLeft.visible = not $topLeft.visible
 					clicked[clickPosition] = shape_idx
