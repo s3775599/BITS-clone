3c0e830cdfb6adcd16f0ba0aac6b0b6042ffb161
Paul <paulatkins88@gmail.com>
2019-10-16 23:34:53 +1100

    Title & About screen

--- NightwatchDialogueSystem/Scenes/Title.gd
+++ NightwatchDialogueSystem/Scenes/Title.gd
@@ -1,7 +1,12 @@
 extends Node2D
+# Refrences
+# Audio SFX from : https://kenney.nl/assets/ui-audio
+# Music file from : https://freesound.org/people/zimbot/sounds/322054/ --- CC Licence
+# Music file from : https://freesound.org/people/GeronimoGeronimo/sounds/338065/ --- CC Licence
 
 func _on_NewGameArea_input_event(viewport, event, shape_idx):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+		$SFXSelect.play()
 		get_tree().change_scene("res://Scenes/Main.tscn")
 
 
@@ -11,6 +16,7 @@ func _on_AboutGameArea_input_event(viewport, event, shape_idx):
 
 func _on_ExitGameArea_input_event(viewport, event, shape_idx):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+		$SFXSelect.play()
 		get_tree().quit()
 
 
@@ -18,18 +24,21 @@ func _on_ExitGameArea_input_event(viewport, event, shape_idx):
 
 func _on_NewGameArea_mouse_entered():
 	$NewGameArea/NewGameSprite.set_modulate(Color('a9a9a9'))
+	$SFXRollOver.play()
 
 func _on_NewGameArea_mouse_exited():
 	$NewGameArea/NewGameSprite.set_modulate(Color(1,1,1))
 
 func _on_AboutGameArea_mouse_entered():
 	$AboutGameArea/AboutGameSprite.set_modulate(Color('a9a9a9'))
+	$SFXRollOver.play()
 
 func _on_AboutGameArea_mouse_exited():
 	$AboutGameArea/AboutGameSprite.set_modulate(Color(1,1,1))
 
 func _on_ExitGameArea_mouse_entered():
 	$ExitGameArea/ExitGameSprite.set_modulate(Color('a9a9a9'))
+	$SFXRollOver.play()
 
 func _on_ExitGameArea_mouse_exited():
 	$ExitGameArea/ExitGameSprite.set_modulate(Color(1,1,1))
