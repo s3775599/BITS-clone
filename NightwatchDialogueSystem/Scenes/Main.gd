98972780542f296c5337abd55a0e82f119ee2629
Michael <michael.seymour@student.rmit.edu.au>
2019-10-16 21:49:03 +1100

    main.gd updates

--- NightwatchDialogueSystem/Scenes/Main.gd
+++ NightwatchDialogueSystem/Scenes/Main.gd
@@ -9,9 +9,7 @@ onready var barry_dialogue = get_node("BarryDialogue")
 
 func _process(delta):
 	if john_close or barry_close:
-		$BackgroundArea/BackgroundSprite.set_modulate(Color('464646'))
-		$BackgroundArea/BarryConsole/BarryConsoleSprite.set_modulate(Color('464646'))
-		$BackgroundArea/JohnConsole/JohnConsoleSprite.set_modulate(Color('464646'))
+		darken_background()
 
 
 func _input(event):
@@ -24,7 +22,7 @@ func _input(event):
 	if john_close or barry_close:
 		can_click = false
 
-	
+
 func _on_BarryConsole_mouse_entered():
 	if not barry_close:
 		$BackgroundArea/BarryConsole/BarryConsoleSprite.set_modulate(Color('4d39df'))
@@ -54,36 +52,18 @@ func _on_JohnConsole_input_event(viewport, event, shape_idx):
 		if barry_close:
 			$BackgroundArea/BarryClose/AnimationPlayer.play('BarryCloseSlideOut')
 			barry_close = false
-		$BackgroundArea/JohnClose/AnimationPlayer.play('JohnCloseSlideIn')
-		john_close = true
-		can_click = false
-		show_john_dialogue()
+		show_john()
+		$CanvasLayer/JohnPopup.set_text(john_dialogue.john_dialogues("1"))
 		
+
 
 func _on_BarryConsole_input_event(viewport, event, shape_idx):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click == true:
 		if john_close:
 			$BackgroundArea/JohnClose/AnimationPlayer.play('BarryCloseSlideOut')
 			barry_close = false
-		$BackgroundArea/BarryClose/AnimationPlayer.play('BarryCloseSlideIn')
-		barry_close = true
-		can_click = false
-		show_barry_dialogue()
-
-
-func show_john_dialogue():
-	$CanvasLayer/JohnPopup.show()
-	$CanvasLayer/JohnPopup.set_text(john_dialogue.john_dialogues("1"))
-
-
-func show_barry_dialogue():
-	$CanvasLayer/BarryPopup.show()
-	$CanvasLayer/BarryPopup.set_text(barry_dialogue.barry_dialogues("1"))
-
-
-func intro():
-	$CanvasLayer/PlayerPopup.show()
-	$CanvasLayer/PlayerPopup.set_text(intro_dialogue.intro_dialogues("1"))
+		show_barry()
+		$CanvasLayer/BarryPopup.set_text(barry_dialogue.barry_dialogues("1"))
 
 
 func _on_BarryPopup_button_id(button_id):
@@ -91,9 +71,8 @@ func _on_BarryPopup_button_id(button_id):
 	if button_id == "end":
 		barry_close = false
 		$CanvasLayer/BarryPopup.hide()
-		$BackgroundArea/BackgroundSprite.set_modulate(Color(1,1,1))
-		$BackgroundArea/BarryConsole/BarryConsoleSprite.set_modulate(Color(1,1,1))
-		$BackgroundArea/JohnConsole/JohnConsoleSprite.set_modulate(Color(1,1,1))
+		normalise_background()
+		can_click = true
 		$BackgroundArea/BarryClose/AnimationPlayer.play('BarryCloseSlideOut')
 	else:
 		var next_dialogue = barry_dialogue.barry_dialogues(button_id)
@@ -106,9 +85,8 @@ func _on_JohnPopup_button_id(button_id):
 	if button_id == "end":
 		john_close = false
 		$CanvasLayer/JohnPopup.hide()
-		$BackgroundArea/BackgroundSprite.set_modulate(Color(1,1,1))
-		$BackgroundArea/BarryConsole/BarryConsoleSprite.set_modulate(Color(1,1,1))
-		$BackgroundArea/JohnConsole/JohnConsoleSprite.set_modulate(Color(1,1,1))
+		normalise_background()
+		can_click = true
 		$BackgroundArea/JohnClose/AnimationPlayer.play('JohnCloseSlideOut')
 	else:
 		var next_dialogue = john_dialogue.john_dialogues(button_id)
@@ -120,18 +98,48 @@ func _on_PlayerPopup_button_id(button_id):
 	print("button_id = ", button_id)
 	if button_id == "end":
 		$CanvasLayer/PlayerPopup.hide()
-		$BackgroundArea/BackgroundSprite.set_modulate(Color(1,1,1))
-		$BackgroundArea/BarryConsole/BarryConsoleSprite.set_modulate(Color(1,1,1))
-		$BackgroundArea/JohnConsole/JohnConsoleSprite.set_modulate(Color(1,1,1))
+		normalise_background()
 	else:
 		intro_dialogue.intro_dialogues(button_id)
 		match button_id:
 			"2":
 				$CanvasLayer/PlayerPopup.hide()
-				$BackgroundArea/BarryClose/AnimationPlayer.play('BarryCloseSlideIn')
-				barry_close = true
-				can_click = false
-				$CanvasLayer/BarryPopup.show()
+				show_barry()
 				$CanvasLayer/BarryPopup.set_text(intro_dialogue.intro_dialogues(button_id))
 
 
+func darken_background():
+	$BackgroundArea/BackgroundSprite.set_modulate(Color('464646'))
+	$BackgroundArea/BarryConsole/BarryConsoleSprite.set_modulate(Color('464646'))
+	$BackgroundArea/JohnConsole/JohnConsoleSprite.set_modulate(Color('464646'))
+	$BackgroundArea/Images/AK47.set_modulate(Color('464646'))
+	$BackgroundArea/Images/RussianFlag.set_modulate(Color('464646'))
+	$BackgroundArea/Images/RussianText.set_modulate(Color('464646'))
+
+
+func normalise_background():
+	$BackgroundArea/BackgroundSprite.set_modulate(Color(1,1,1))
+	$BackgroundArea/BarryConsole/BarryConsoleSprite.set_modulate(Color(1,1,1))
+	$BackgroundArea/JohnConsole/JohnConsoleSprite.set_modulate(Color(1,1,1))
+	$BackgroundArea/Images/AK47.set_modulate(Color(1,1,1))
+	$BackgroundArea/Images/RussianFlag.set_modulate(Color(1,1,1))
+	$BackgroundArea/Images/RussianText.set_modulate(Color(1,1,1))
+
+
+func show_barry():
+	$BackgroundArea/BarryClose/AnimationPlayer.play('BarryCloseSlideIn')
+	barry_close = true
+	can_click = false
+	$CanvasLayer/BarryPopup.show()
+
+
+func show_john():
+	$BackgroundArea/JohnClose/AnimationPlayer.play('JohnCloseSlideIn')
+	john_close = true
+	can_click = false
+	$CanvasLayer/JohnPopup.show()
+
+
+func intro():
+	$CanvasLayer/PlayerPopup.show()
+	$CanvasLayer/PlayerPopup.set_text(intro_dialogue.intro_dialogues("1"))
