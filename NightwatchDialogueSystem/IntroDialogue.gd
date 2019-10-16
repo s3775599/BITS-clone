245fb3ff2b93cce7b87d65e98057954ea1e781ae
Michael <michael.seymour@student.rmit.edu.au>
2019-10-16 23:04:53 +1100

    Intro dialogue system working

--- NightwatchDialogueSystem/IntroDialogue.gd
+++ NightwatchDialogueSystem/IntroDialogue.gd
@@ -1,30 +1,70 @@
 extends Node
-
-var barry_close = false
-var john_close = false
-
-
-func _process(delta):
-	if john_close or barry_close:
-		$BackgroundArea/BackgroundSprite.set_modulate(Color('464646'))
-		$BackgroundArea/BarryConsole/BarryConsoleSprite.set_modulate(Color('464646'))
-		$BackgroundArea/JohnConsole/JohnConsoleSprite.set_modulate(Color('464646'))
-
+onready var main_script = get_node("/root/Main")
+onready var player_popup = get_node("/root/Main/CanvasLayer/PlayerPopup")
+onready var barry_popup = get_node("/root/Main/CanvasLayer/BarryPopup")
+onready var john_popup = get_node("/root/Main/CanvasLayer/JohnPopup")
+onready var barry_anim = get_node("/root/Main/BackgroundArea/BarryClose/AnimationPlayer")
+onready var john_anim = get_node("/root/Main/BackgroundArea/JohnClose/AnimationPlayer")
 
 func intro_dialogues(id):
 	match id:
-		"1":
-			return [["PLAYER: . . .scanning Quadrant 3, 216 to 322 degrees. Delta V looks good. Report on Quadrant 1, how are we looking Barry?", "[ -- continue -- ]"], ["0", "2"]]
-		"2":
-			return [["Everything's fine mate.", "[ -- continue -- ]"], ["0", "3"]]
-
+		"i1":
+			return [["PLAYER: . . .scanning Quadrant 3, 216 to 322 degrees. Delta V looks good. Report on Quadrant 1, how are we looking Barry?", "[ -- continue -- ]"], ["0", "i2"]]
+		"i2":
+			return [["Everything's fine mate.", "[ -- continue -- ]"], ["0", "i3"]]
+		"i3":
+			return [["PLAYER: I beg you pardon, Barry?", "[ -- continue --]"], ["0", "i4"]]
+		"i4":
+			return [["All good.", "[ -- continue --]"], ["0", "i5"]]
+		"i5":
+			return [["PLAYER: Barry, I know we've been out here a long time but that doesn't give you license to drop protocol whenever you feel like it. Got it?", "[ -- continue -- ]"], ["0", "i6"]]
+		"i6":
+			return [["{sigh} Yes [i]SIR[/i] Lieutenant [i]SIR[/i] sorry [i]SIR[/i].", "[ -- continue -- ]"], ["0", "i7"]]
+		"i7":
+			return [["Systems report, John?", "[ -- continue -- ]"], ["0", "i8"]]
+		"i8":
+			return [["It's fine, it's whatever.", "[ -- continue -- ]"], ["0", "i9"]]
 
 func intro_sequence(button_id):
+	intro_dialogues(button_id)
 	match button_id:
-			"2":
-				$CanvasLayer/PlayerPopup.hide()
-				$BackgroundArea/BarryClose/AnimationPlayer.play('BarryCloseSlideIn')
-				barry_close = true
-#				can_click = false
-				$CanvasLayer/BarryPopup.show()
-				intro_dialogues(button_id)
+		"i2":
+			barry_anim.play('BarryCloseSlideIn')
+			player_to_barry(button_id)
+		"i3":
+			barry_to_player(button_id)
+		"i4":
+			player_to_barry(button_id)
+		"i5":
+			barry_to_player(button_id)
+		"i6":
+			player_to_barry(button_id)
+		"i7":
+			barry_anim.play('BarryCloseSlideOut')
+			barry_to_player(button_id)
+		"i8":
+			john_anim.play('JohnCloseSlideIn')
+			player_to_john(button_id)
+
+
+func player_to_barry(button_id):
+	player_popup.hide()
+	barry_popup.show()
+	barry_popup.set_text(intro_dialogues(button_id))
+
+
+func player_to_john(button_id):
+	player_popup.hide()
+	john_popup.show()
+	john_popup.set_text(intro_dialogues(button_id))
+
+
+func barry_to_player(button_id):
+	barry_popup.hide()
+	player_popup.show()
+	player_popup.set_text(intro_dialogues(button_id))
+
+func john_to_player(button_id):
+	john_popup.hide()
+	player_popup.show()
+	player_popup.set_text(intro_dialogues(button_id))
--- NightwatchDialogueSystem/Scenes/Main.gd
+++ NightwatchDialogueSystem/Scenes/Main.gd
@@ -3,13 +3,16 @@ extends Control
 var can_click = true
 var john_close = false
 var barry_close = false
+var player_close = false
 onready var intro_dialogue = get_node("IntroDialogue")
 onready var john_dialogue = get_node("JohnDialogue")
 onready var barry_dialogue = get_node("BarryDialogue")
 
 func _process(delta):
-	if john_close or barry_close:
+	if john_close or barry_close or player_close:
 		darken_background()
+	else:
+		normalise_background()
 
 
 func _input(event):
@@ -19,7 +22,7 @@ func _input(event):
 		$BackgroundArea/BarryConsole/AnimationPlayer.play('BarryConsoleFastIn')
 	if Input.is_action_just_pressed("ui_down"):
 		intro()
-	if john_close or barry_close:
+	if john_close or barry_close or player_close:
 		can_click = false
 
 
@@ -50,18 +53,15 @@ func _on_JohnConsole_mouse_exited():
 func _on_JohnConsole_input_event(viewport, event, shape_idx):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click == true:
 		if barry_close:
-			$BackgroundArea/BarryClose/AnimationPlayer.play('BarryCloseSlideOut')
-			barry_close = false
+			hide_barry()
 		show_john()
 		$CanvasLayer/JohnPopup.set_text(john_dialogue.john_dialogues("1"))
-		
 
 
 func _on_BarryConsole_input_event(viewport, event, shape_idx):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click == true:
 		if john_close:
-			$BackgroundArea/JohnClose/AnimationPlayer.play('BarryCloseSlideOut')
-			barry_close = false
+			hide_john()
 		show_barry()
 		$CanvasLayer/BarryPopup.set_text(barry_dialogue.barry_dialogues("1"))
 
@@ -69,43 +69,43 @@ func _on_BarryConsole_input_event(viewport, event, shape_idx):
 func _on_BarryPopup_button_id(button_id):
 	print("button_id = ", button_id)
 	if button_id == "end":
-		barry_close = false
-		$CanvasLayer/BarryPopup.hide()
-		normalise_background()
-		can_click = true
-		$BackgroundArea/BarryClose/AnimationPlayer.play('BarryCloseSlideOut')
+		hide_barry()
+	elif button_id[0] == "i":
+		intro_dialogue.intro_sequence(button_id)
 	else:
 		var next_dialogue = barry_dialogue.barry_dialogues(button_id)
-		print("next_dialogue = ", next_dialogue)
 		$CanvasLayer/BarryPopup.set_text(next_dialogue)
 
 
 func _on_JohnPopup_button_id(button_id):
 	print("button_id = ", button_id)
 	if button_id == "end":
-		john_close = false
-		$CanvasLayer/JohnPopup.hide()
-		normalise_background()
-		can_click = true
-		$BackgroundArea/JohnClose/AnimationPlayer.play('JohnCloseSlideOut')
+		hide_john()
 	else:
 		var next_dialogue = john_dialogue.john_dialogues(button_id)
-		print("next_dialogue = ", next_dialogue)
 		$CanvasLayer/JohnPopup.set_text(next_dialogue)
 
 
 func _on_PlayerPopup_button_id(button_id):
-	print("button_id = ", button_id)
 	if button_id == "end":
-		$CanvasLayer/PlayerPopup.hide()
-		normalise_background()
+		hide_player()
 	else:
-		intro_dialogue.intro_dialogues(button_id)
-		match button_id:
-			"2":
-				$CanvasLayer/PlayerPopup.hide()
-				show_barry()
-				$CanvasLayer/BarryPopup.set_text(intro_dialogue.intro_dialogues(button_id))
+		intro_dialogue.intro_sequence(button_id)
+#		intro_dialogue.intro_dialogues(button_id)
+#		match button_id:
+#			"i2":
+#				$CanvasLayer/PlayerPopup.hide()
+#				show_barry()
+#				$CanvasLayer/BarryPopup.set_text(intro_dialogue.intro_dialogues(button_id))
+
+
+#func intro_sequence(button_id):
+#	intro_dialogue.intro_dialogues(button_id)
+#	match button_id:
+#		"i2":
+#			$CanvasLayer/PlayerPopup.hide()
+#			show_barry()
+#			$CanvasLayer/BarryPopup.set_text(intro_dialogue.intro_dialogues(button_id))
 
 
 func darken_background():
@@ -140,6 +140,32 @@ func show_john():
 	$CanvasLayer/JohnPopup.show()
 
 
-func intro():
+func show_player():
+	player_close = true
+	can_click = false
 	$CanvasLayer/PlayerPopup.show()
-	$CanvasLayer/PlayerPopup.set_text(intro_dialogue.intro_dialogues("1"))
+
+
+func hide_barry():
+	$CanvasLayer/BarryPopup.hide()
+	$BackgroundArea/BarryClose/AnimationPlayer.play('BarryCloseSlideOut')
+	barry_close = false
+	can_click = true
+
+
+func hide_john():
+	$CanvasLayer/JohnPopup.hide()
+	$BackgroundArea/JohnClose/AnimationPlayer.play('JohnCloseSlideOut')
+	john_close = false
+	can_click = true
+
+
+func hide_player():
+	$CanvasLayer/PlayerPopup.hide()
+	player_close = false
+	can_click = true
+
+
+func intro():
+	show_player()
+	$CanvasLayer/PlayerPopup.set_text(intro_dialogue.intro_dialogues("i1"))
