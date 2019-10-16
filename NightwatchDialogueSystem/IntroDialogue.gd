812b66f0c3b48d1047cca1f164bbd65d3cba72e5
Michael <michael.seymour@student.rmit.edu.au>
2019-10-17 08:21:37 +1100

    Intro dialogue updates

--- NightwatchDialogueSystem/IntroDialogue.gd
+++ NightwatchDialogueSystem/IntroDialogue.gd
@@ -1,5 +1,6 @@
 extends Node
 
+# gets Nodes from the main scene as variables
 onready var main_script = get_node("/root/Main")
 onready var player_popup = get_node("/root/Main/CanvasLayer/PlayerPopup")
 onready var barry_popup = get_node("/root/Main/CanvasLayer/BarryPopup")
@@ -7,32 +8,53 @@ onready var john_popup = get_node("/root/Main/CanvasLayer/JohnPopup")
 onready var barry_anim = get_node("/root/Main/BackgroundArea/BarryClose/AnimationPlayer")
 onready var john_anim = get_node("/root/Main/BackgroundArea/JohnClose/AnimationPlayer")
 
+# all the intro dialogue lines
 func intro_dialogues(id):
 	match id:
 		"i1":
-			return [["PLAYER: . . . scanning Quadrant 3, 216 to 322 degrees. Delta V looks good. Report on Quadrant 1, how are we looking Barry?", "[ -- continue -- ]"], ["0", "i2"]]
+			return [["", ". . . scanning Quadrant 3, 216 to 322 degrees. Delta V looks good. Report on Quadrant 1, how are we looking Barry?"], ["0", "i2"]]
 		"i2":
 			return [["Everything's fine mate.", "[ -- continue -- ]"], ["0", "i3"]]
 		"i3":
-			return [["PLAYER: I beg your pardon, Barry?", "[ -- continue --]"], ["0", "i4"]]
+			return [["", "I beg your pardon, Barry?"], ["0", "i4"]]
 		"i4":
 			return [["All good.", "[ -- continue --]"], ["0", "i5"]]
 		"i5":
-			return [["PLAYER: Barry, I know we've been out here a long time but that doesn't give you license to drop protocol whenever you feel like it. Got it?", "[ -- continue -- ]"], ["0", "i6"]]
+			return [["", "Barry, I know we've been out here a long time but that doesn't give you license to drop protocol whenever you feel like it. Got it?"], ["0", "i6"]]
 		"i6":
 			return [["{sigh} Yes [i]SIR[/i] Lieutenant [i]SIR[/i] sorry [i]SIR[/i].", "[ -- continue -- ]"], ["0", "i7"]]
 		"i7":
-			return [["Systems report, John?", "[ -- continue -- ]"], ["0", "i8"]]
+			return [["", "Systems report, John?"], ["0", "i8"]]
 		"i8":
-			return [["It's fine, it's whatever.", "[ -- continue -- ]"], ["0", "end"]]
+			return [["It's fine, it's whatever.", "[ -- continue -- ]"], ["0", "i9"]]
+		"i9":
+			return [["", "JOHN."], ["0", "i10"]]
+		"i10":
+			return [["What? What do you want me to say? It's exactly the same as it was last shift. We haven't done anything since the last shift. It's all the same as the last shift.", "[ -- continue -- ]"], ["0", "i11"]]
+		"i11":
+			return [["", "What's gotten into you two?", "One more outburst like that and I'll have to file a report of insubordination!", "Are you sure nothing's changed?", "Wee that's good to know."], ["0", "i11a", "i11b", "i11c", "i11d"]]
+		"i11a":
+			return [["Nothing.", "[ -- continue -- ]"], ["0", "i11a1"]]
+		"i11a1":
+			return [["Nothing.", "[ -- continue -- ]"], ["0", "i11r"]]
+		"i11r":
+			return [["", "What's gotten into you two?", "One more outburst like that and I'll have to file a report of insubordination!", "Are you sure nothing's changed?", "Wee that's good to know."], ["0", "i11a", "i11b", "i11c", "i11d"]]
+		"i11b":
+			return [["Oh no, not that. Anything but that.", "[ -- continue -- ]"], ["0", "i11b1"]]
+		"i11c":
+			return
+		"i11d":
+			return
 
+# the sequence of dialogue and animations/transitions in the intro
 func intro_sequence(button_id):
-	intro_dialogues(button_id)
 	match button_id:
 		"i2":
 			barry_anim.play('BarryCloseSlideIn')
+			# the previous dialogue and current dialogue is from the Player to Barry
 			player_to_barry(button_id)
 		"i3":
+			# the previous dialogue and current dialogue is from Barry to the Player
 			barry_to_player(button_id)
 		"i4":
 			player_to_barry(button_id)
@@ -44,31 +66,65 @@ func intro_sequence(button_id):
 			barry_anim.play('BarryCloseSlideOut')
 			barry_to_player(button_id)
 		"i8":
+			# the previous dialogue and current dialogue is from the Player to John
 			john_anim.play('JohnCloseSlideIn')
 			player_to_john(button_id)
-			main_script.john_close = false
-			main_script.barry_close = false
-			main_script.player_close = false
+		"i9":
+			john_to_player(button_id)
+		"i10":
+			player_to_john(button_id)
+		"i11":
+			john_anim.play('JohnCloseSlideOut')
+			john_to_player(button_id)
+		"i11a":
+			john_anim.play('JohnCloseSlideIn')
+			player_to_john(button_id)
+		"i11a1":
+			john_anim.play('JohnCloseSlideOut')
+			john_to_barry(button_id)
+			barry_anim.play('BarryCloseSlideIn')
+		"i11r":
+			barry_anim.play('BarryCloseSlideOut')
+			barry_to_player(button_id)
+		"i11b":
+			john_anim.play('JohnCloseSlideIn')
+			player_to_john(button_id)
+		
+		
+		
+#			main_script.john_close = false
+#			main_script.barry_close = false
+#			main_script.player_close = false
 
 
+# sets the dialogue box transition from the Player to Barry
 func player_to_barry(button_id):
 	player_popup.hide()
 	barry_popup.show()
 	barry_popup.set_text(intro_dialogues(button_id))
 
 
+# sets the dialogue box transition from the Player to John
 func player_to_john(button_id):
 	player_popup.hide()
 	john_popup.show()
 	john_popup.set_text(intro_dialogues(button_id))
 
 
+# sets the dialogue box transition from Barry to the Player
 func barry_to_player(button_id):
 	barry_popup.hide()
 	player_popup.show()
 	player_popup.set_text(intro_dialogues(button_id))
 
+
+# sets the dialogue box transition from John to the Player
 func john_to_player(button_id):
 	john_popup.hide()
 	player_popup.show()
 	player_popup.set_text(intro_dialogues(button_id))
+
+func john_to_barry(button_id):
+	john_popup.hide()
+	barry_popup.show()
+	barry_popup.set_text(intro_dialogues(button_id))
--- NightwatchDialogueSystem/Scenes/Main.gd
+++ NightwatchDialogueSystem/Scenes/Main.gd
@@ -1,29 +1,41 @@
 extends Control
 
+# checks is a dialogue can be initiated (i.e. none already active)
 var can_click = true
+
+# booleans to identify current active dialogue
 var john_close = false
 var barry_close = false
 var player_close = false
+
+# Godot's way of storing a Node as a variable
 onready var intro_dialogue = get_node("IntroDialogue")
 onready var john_dialogue = get_node("JohnDialogue")
 onready var barry_dialogue = get_node("BarryDialogue")
 
+
+# this function updates every frame
 func _process(delta):
+	# darkens background when a dialogue is active
 	if john_close or barry_close or player_close:
 		darken_background()
-
-
-func _input(event):
-	if Input.is_action_just_pressed("ui_right"):
-		$BackgroundArea/BarryConsole/AnimationPlayer.play('BarryConsoleSlowOut')
-	if Input.is_action_just_pressed("ui_left"):
-		$BackgroundArea/BarryConsole/AnimationPlayer.play('BarryConsoleFastIn')
-	if Input.is_action_just_pressed("ui_down"):
-		intro()
-	if john_close or barry_close or player_close:
 		can_click = false
 
 
+# checks all input signals
+func _input(event):
+	# initiates Barry dialogue
+	if Input.is_action_just_pressed("ui_right"):
+		$BackgroundArea/BarryConsole/AnimationPlayer.play('BarryConsoleSlowOut')
+	#initiates John dialogue
+	if Input.is_action_just_pressed("ui_left"):
+		$BackgroundArea/BarryConsole/AnimationPlayer.play('BarryConsoleFastIn')
+	# initiates Intro dialogue
+	if Input.is_action_just_pressed("ui_down"):
+		intro()
+
+
+# mouseover signal on Barry at the console
 func _on_BarryConsole_mouse_entered():
 	if not barry_close:
 		$BackgroundArea/BarryConsole/BarryConsoleSprite.set_modulate(Color('4d39df'))
@@ -31,11 +43,13 @@ func _on_BarryConsole_mouse_entered():
 		can_click = false
 
 
+# mouseoff signal on Barry at the console
 func _on_BarryConsole_mouse_exited():
 	$BackgroundArea/BarryConsole/BarryConsoleSprite.set_modulate(Color(1, 1, 1))
 	can_click = true
 
 
+# mouseover signal on John at the console
 func _on_JohnConsole_mouse_entered():
 	if not john_close:
 		$BackgroundArea/JohnConsole/JohnConsoleSprite.set_modulate(Color('4d39df'))
@@ -43,11 +57,13 @@ func _on_JohnConsole_mouse_entered():
 		can_click = false
 
 
+# mouseoff signal on John at the console
 func _on_JohnConsole_mouse_exited():
 	$BackgroundArea/JohnConsole/JohnConsoleSprite.set_modulate(Color(1, 1, 1))
 	can_click = true
 
 
+# mouse click signals on John at the console
 func _on_JohnConsole_input_event(viewport, event, shape_idx):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click == true:
 		if barry_close:
@@ -56,6 +72,7 @@ func _on_JohnConsole_input_event(viewport, event, shape_idx):
 		$CanvasLayer/JohnPopup.set_text(john_dialogue.john_dialogues("1"))
 
 
+# mouse click signals on John at the console
 func _on_BarryConsole_input_event(viewport, event, shape_idx):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click == true:
 		if john_close:
@@ -64,6 +81,7 @@ func _on_BarryConsole_input_event(viewport, event, shape_idx):
 		$CanvasLayer/BarryPopup.set_text(barry_dialogue.barry_dialogues("1"))
 
 
+# collects id of pressed button from Barry dialogue
 func _on_BarryPopup_button_id(button_id):
 	print("button_id = ", button_id)
 	if button_id == "end":
@@ -76,17 +94,22 @@ func _on_BarryPopup_button_id(button_id):
 		$CanvasLayer/BarryPopup.set_text(next_dialogue)
 
 
+# collects id of pressed button from John dialogue
 func _on_JohnPopup_button_id(button_id):
 	print("button_id = ", button_id)
 	if button_id == "end":
 		hide_john()
 		normalise_background()
+	elif button_id[0] == "i":
+		intro_dialogue.intro_sequence(button_id)
 	else:
 		var next_dialogue = john_dialogue.john_dialogues(button_id)
 		$CanvasLayer/JohnPopup.set_text(next_dialogue)
 
 
+# collects id of pressed button from Player dialogue
 func _on_PlayerPopup_button_id(button_id):
+	# ends the dialogue if the last button signal was "end"
 	if button_id == "end":
 		if player_close:
 			hide_player()
@@ -96,9 +119,11 @@ func _on_PlayerPopup_button_id(button_id):
 			hide_barry()
 		normalise_background()
 	else:
+		# calls the next line of dialogue
 		intro_dialogue.intro_sequence(button_id)
 
 
+# makes the background dark
 func darken_background():
 	$BackgroundArea/BackgroundSprite.set_modulate(Color('464646'))
 	$BackgroundArea/BarryConsole/BarryConsoleSprite.set_modulate(Color('464646'))
@@ -108,6 +133,7 @@ func darken_background():
 	$BackgroundArea/Images/RussianText.set_modulate(Color('464646'))
 
 
+# restores the darkened background
 func normalise_background():
 	$BackgroundArea/BackgroundSprite.set_modulate(Color(1,1,1))
 	$BackgroundArea/BarryConsole/BarryConsoleSprite.set_modulate(Color(1,1,1))
@@ -117,6 +143,7 @@ func normalise_background():
 	$BackgroundArea/Images/RussianText.set_modulate(Color(1,1,1))
 
 
+# shows Barry's close-up and dialogue box
 func show_barry():
 	$BackgroundArea/BarryClose/AnimationPlayer.play('BarryCloseSlideIn')
 	barry_close = true
@@ -124,6 +151,7 @@ func show_barry():
 	$CanvasLayer/BarryPopup.show()
 
 
+# shows John's close-up and dialogue box
 func show_john():
 	$BackgroundArea/JohnClose/AnimationPlayer.play('JohnCloseSlideIn')
 	john_close = true
@@ -131,12 +159,14 @@ func show_john():
 	$CanvasLayer/JohnPopup.show()
 
 
+# shows player dialogue box
 func show_player():
 	player_close = true
 	can_click = false
 	$CanvasLayer/PlayerPopup.show()
 
 
+# hides Barry's close-up and dialogue box
 func hide_barry():
 	$CanvasLayer/BarryPopup.hide()
 	$BackgroundArea/BarryClose/AnimationPlayer.play('BarryCloseSlideOut')
@@ -144,6 +174,7 @@ func hide_barry():
 	can_click = true
 
 
+# hides John's close-up and dialogue box
 func hide_john():
 	$CanvasLayer/JohnPopup.hide()
 	$BackgroundArea/JohnClose/AnimationPlayer.play('JohnCloseSlideOut')
@@ -151,12 +182,15 @@ func hide_john():
 	can_click = true
 
 
+# hides Player's dialogue box
 func hide_player():
 	$CanvasLayer/PlayerPopup.hide()
 	player_close = false
 	can_click = true
 
 
+# initiates the Intro dialogue
 func intro():
 	show_player()
+	# calls the first Intro dialogue lines from the intro_dialogue script
 	$CanvasLayer/PlayerPopup.set_text(intro_dialogue.intro_dialogues("i1"))
